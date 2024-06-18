9de2458469f28429739dc835143a874163388dfe
farman1094 <farman1761md@gmail.com>
2024-06-18 23:08:04 +0530

    chore: forge init

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+
+
+    function setup() external{
+        FundMe fundMe = new FundMe();
+    }
+    function testDemo() public {}
+    }
